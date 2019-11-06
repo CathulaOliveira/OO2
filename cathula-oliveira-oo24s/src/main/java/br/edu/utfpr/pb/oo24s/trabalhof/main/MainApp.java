311237887265a267485b5451e2ac5ce5313b2c62
Cathula Oliveira <cathulapameladeoliveira@gmail.com>
2019-11-06 19:24:47 -0300

    commit inicial do projeto

---
+++ cathula-oliveira-oo24s/src/main/java/br/edu/utfpr/pb/oo24s/trabalhof/main/MainApp.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.pb.oo24s.trabalhof.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
